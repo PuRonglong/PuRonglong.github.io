b2c0caec9da0890b461dc04008db4209c46a12a3
puronglong <1240429156@qq.com>
2016-01-07 19:37:06 +0800

    no message

--- my-blog/js/blogApp.js
+++ my-blog/js/blogApp.js
@@ -2,7 +2,18 @@ define(['jquery', 'angular'], function($, angular){
 	var puronglong = angular.module('puronglong', ['ui.router', 'ngAnimate']);//定义了一个模块,后面是依赖列表
 
 	puronglong.config(['$stateProvider', function($stateProvider){
-		$stateProvider.
-			state();
+		$stateProvider
+			.state('main', {
+				url: '/',
+				templateUrl: './template/blog-main.html'
+			})
+			.state('home', {
+				url: '',
+				templateUrl: './template/blog-main.html'
+			})
+			.state('list', {
+				url: '/list',
+				templateUrl: './template/blog-list.html'
+			})
 	}]);
 })
