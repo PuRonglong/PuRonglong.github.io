d36b4acb46e49a012391b1ad70e8d8023dd63890
puronglong <1240429156@qq.com>
2016-02-14 17:26:32 +0800

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
-gulp.task('style.min', function(){
-
+gulp.task('styleCss', function(){
+	return gulp.src('./css/style.css')
+		.pipe(minifyCss())
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('./css/lib'));
 });
+
+gulp.task('blogStyleCss', ['styleCss']);
