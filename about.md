1e4104a07d3e0c5df4b763d42dda92ea5148d24d
puronglong <1240429156@qq.com>
2016-08-08 20:27:19 +0800

    move blog

--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: About
-permalink: /about/
----
-
-[不为谁而写的博](http://www.puronglong.com/2016/06/08/whrite-blog.html)
---
+++ js/index.js
@@ -0,0 +1,28 @@
+/*!
+ * author: puronglong
+ */
+
+$(document).ready(function () {
+	'use strict';
+	$('.slider').slider({
+		full_width: true,
+		indicators: true,  // 导航标识
+		interval: 8000,   // 间隔时间设置
+		transition: 700    // 持续时间设置
+	});
+
+	$('.button-previous-left').on('click', function () {
+		$('.slider').slider('prev');
+	});
+
+	$('.button-previous-right').on('click', function () {
+		$('.slider').slider('next');
+	});
+
+	setTimeout(function () {
+		$('#menu').addClass('menu animated bounceInDown');
+	}, 1000);
+});
+
+
+
